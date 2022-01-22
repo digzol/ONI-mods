746b89a6f33f16e92f6473a6557f292d6c9ae661
NSavard <digzol@users.noreply.github.com>
2022-01-22 01:09:16 -0500

    Added artifact display

--- SpacePOIExtraInfo/SpacePOIExtraInfo.cs
+++ SpacePOIExtraInfo/SpacePOIExtraInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using HarmonyLib;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 namespace SpacePOIExtraInfo
@@ -156,5 +157,106 @@ namespace SpacePOIExtraInfo
                 }
             }
         }
+    
+        // Hook into the method responsible for refreshing artifact info at the POI and inject our own code:
+        // * Edit the artifact row to display the actual artifact.
+        // * Enable text-wrapping so long artifact names can fit without overlapping.
+        // * Bump the time to recharge to a new line to make it more consistant and have the text fit.
+        [HarmonyPatch(typeof(SpacePOISimpleInfoPanel))]
+        [HarmonyPatch("RefreshArtifacts")]
+        public class SpacePOIArtifactInfoPatch
+        {
+            public static LocString ARTIFACT_AVAILABLE = "Available";
+            public static LocString ARTIFACT_UNAVAILABLE = "Unavailable";
+            public static LocString ARTIFACT_RECHARGE_LABEL = "<b>Recharge Time</b>";
+
+            private static SpacePOISimpleInfoPanel SpacePOIInfoPanel;
+            private static GameObject RechargeTimeRow;
+
+            public static void Postfix(ref SpacePOISimpleInfoPanel __instance,
+                ArtifactPOIConfigurator artifactConfigurator,
+                CollapsibleDetailContentPanel spacePOIPanel)
+            {
+                // Add UI changes if they weren't set yet or the info panel got set to a new instance somehow
+                if (!ReferenceEquals(SpacePOIInfoPanel, __instance))
+                {
+                    SimpleInfoScreen simpleInfoScreen = Traverse.Create(__instance).Field("simpleInfoRoot").GetValue() as SimpleInfoScreen;
+                    GameObject iconLabelRow = simpleInfoScreen.iconLabelRow;
+                    GameObject parentContainer = spacePOIPanel.Content.gameObject;
+
+                    SpacePOIInfoPanel = __instance;
+                    RechargeTimeRow = Util.KInstantiateUI(iconLabelRow, parentContainer);
+
+                    // Setup word wrapping - this could really be optimized
+                    GameObject artifactRow = Traverse.Create(__instance).Field("artifactRow").GetValue() as GameObject;
+                    HierarchyReferences refs = artifactRow.GetComponent<HierarchyReferences>();
+                    refs.GetReference<LocText>("NameLabel").enableWordWrapping = true;
+                    refs.GetReference<LocText>("NameLabel").gameObject.AddComponent<LayoutElement>().flexibleWidth = 1;
+                    refs.GetReference<LocText>("ValueLabel").gameObject.GetComponent<LayoutElement>().ignoreLayout = false;
+                }
+
+                // Force the ui element to the bottom in case new element rows were added
+                RechargeTimeRow.rectTransform().SetAsLastSibling();
+
+                var smi = artifactConfigurator.GetSMI<ArtifactPOIStates.Instance>();
+
+                string uniqueArtifactID = smi.configuration.GetArtifactID();
+                bool isDestroyedOnHarvest = smi.configuration.DestroyOnHarvest();
+
+                //int numHarvests = (int)Traverse.Create(smi).Field("numHarvests").GetValue();
+
+                // Destroyable POI with a pre-assigned artifact (e.g. Russell's teapot)
+                if (!string.IsNullOrEmpty(uniqueArtifactID) && isDestroyedOnHarvest)
+                {
+                    GameObject artifactRow = Traverse.Create(__instance).Field("artifactRow").GetValue() as GameObject;
+                    GameObject artifactPrefab = Assets.GetPrefab(uniqueArtifactID);
+
+                    if (artifactPrefab == null)
+                        return;
+
+                    HierarchyReferences refs = artifactRow.GetComponent<HierarchyReferences>();
+                    refs.GetReference<LocText>("NameLabel").text = artifactPrefab.GetProperName();
+
+                    var uisprite = Def.GetUISprite(uniqueArtifactID);
+                    if (uisprite != null)
+                    {
+                        refs.GetReference<Image>("Icon").sprite = uisprite.first;
+                        refs.GetReference<Image>("Icon").color = uisprite.second;
+                    }
+                }
+                // Rechargeable POI with its next artifact assigned - a new artifact is generated each time one is harvested
+                else if (!string.IsNullOrEmpty(smi.artifactToHarvest))
+                {
+                    GameObject artifactRow = Traverse.Create(__instance).Field("artifactRow").GetValue() as GameObject;
+                    GameObject artifactPrefab = Assets.GetPrefab(smi.artifactToHarvest);
+
+                    if (artifactPrefab == null)
+                        return;
+
+                    bool canHarvestArtifact = smi.CanHarvestArtifact();
+
+                    HierarchyReferences refs = artifactRow.GetComponent<HierarchyReferences>();
+                    refs.GetReference<LocText>("NameLabel").text = artifactPrefab.GetProperName();
+                    refs.GetReference<LocText>("ValueLabel").text = (canHarvestArtifact) ? ARTIFACT_AVAILABLE : ARTIFACT_UNAVAILABLE;
+
+                    var uisprite = Def.GetUISprite(smi.artifactToHarvest);
+                    if (uisprite != null)
+                    {
+                        refs.GetReference<Image>("Icon").sprite = uisprite.first;
+                        refs.GetReference<Image>("Icon").color = uisprite.second;
+                    }
+
+                    RechargeTimeRow.SetActive(!canHarvestArtifact);
+                    if (!canHarvestArtifact)
+                    {
+                        refs = RechargeTimeRow.GetComponent<HierarchyReferences>();
+                        refs.GetReference<LocText>("NameLabel").text = ARTIFACT_RECHARGE_LABEL;
+                        refs.GetReference<LocText>("ValueLabel").text = GameUtil.GetFormattedCycles(smi.RechargeTimeRemaining(), forceCycles: true);
+                        refs.GetReference<LocText>("ValueLabel").alignment = TextAlignmentOptions.MidlineRight;
+                    }
+                }
+            }
+        }
     }
 }
+
